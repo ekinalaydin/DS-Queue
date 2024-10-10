4d4616c83d8ef78cec8c1737ff3e9219d3fc6324
ekinalaydin <ekinaly06@gmail.com>
2024-10-11 00:28:57 +0300

    Add Stack push method

---
+++ DS-Queue-ArrayList/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack();
+
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+
+        myStack.printStack();
+
+
+    }
+
+}
